7701ed51e949e70227430fc09aeccb69a8a490df
projectbykd-jpg <projectbykd@gmail.com>
2026-08-08 15:09:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.TYPEFORGE_CONFIG = {
   API_URL: "https://script.google.com/macros/s/AKfycbx7OrCxPHHbLfTV3z18J5HqscI8b1GighAVi5wQK6adNBy5MeL2M6qgMjFmmq1KmELqtg/exec",
-  POLL_MS: 1800
+  POLL_MS: 1400
 };
