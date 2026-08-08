fe80b68f1d75efb8207ba5b8dd830c8226a76e5d
projectbykd-jpg <projectbykd@gmail.com>
2026-08-08 12:58:37 +0700

    Update API_URL to the new Google Apps Script URL

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Tempel URL deployment Google Apps Script Web App di bawah ini.
 // Contoh: https://script.google.com/macros/s/AKfycb.../exec
 window.TYPEFORGE_CONFIG = {
-  API_URL: "PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE"
+  API_URL: "https://script.google.com/macros/s/AKfycbx7OrCxPHHbLfTV3z18J5HqscI8b1GighAVi5wQK6adNBy5MeL2M6qgMjFmmq1KmELqtg/exec"
 };
