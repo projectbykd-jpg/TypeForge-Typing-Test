c9d0c4bc350bfc6c3e26ad34c77583b7ef952bb2
projectbykd-jpg <projectbykd@gmail.com>
2026-08-08 12:53:57 +0700

    Update and rename Code.gs to config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// Tempel URL deployment Google Apps Script Web App di bawah ini.
+// Contoh: https://script.google.com/macros/s/AKfycb.../exec
+window.TYPEFORGE_CONFIG = {
+  API_URL: "PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE"
+};
