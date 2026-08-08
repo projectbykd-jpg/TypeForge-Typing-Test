fa8ecb4378ac747d58e2c5a93e65c6e62c97a073
projectbykd-jpg <projectbykd@gmail.com>
2026-08-08 13:24:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
-// Tempel URL deployment Google Apps Script Web App di bawah ini.
-// Contoh: https://script.google.com/macros/s/AKfycb.../exec
 window.TYPEFORGE_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbx7OrCxPHHbLfTV3z18J5HqscI8b1GighAVi5wQK6adNBy5MeL2M6qgMjFmmq1KmELqtg/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbx7OrCxPHHbLfTV3z18J5HqscI8b1GighAVi5wQK6adNBy5MeL2M6qgMjFmmq1KmELqtg/exec",
+  POLL_MS: 1800
 };
